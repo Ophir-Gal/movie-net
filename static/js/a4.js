fe426d53b38fe7fe6cebb2e17caeb15ce4b95a19
ophir <ophirgal2@gmail.com>
2020-04-14 23:27:31 -0400

    added svg file and added comments to a4.js

--- static/js/a4.js
+++ static/js/a4.js
@@ -1,10 +1,21 @@
+var calledRenderFunc = false // a flag to ensure not to render viz twice
+
 // Fetch data from the server and render visualization
 fetch('http://localhost:8080/').then(function(response) { 
-  response.json().then(data => {
-    renderNetworkViz(data.nodes, data.links);
+  response.json().then(data => {  
+    if (!calledRenderFunc) {
+      renderNetworkViz(data.nodes, data.links);
+      calledRenderFunc = true;
+    }
   })
   .catch(e => console.log(e));
 })
+
+/**
+ * ============================================================================
+ * ======================= Begin Utility Functions ============================
+ * ============================================================================
+ */
 
 function getNeighbors(node) {
   return links.reduce(function (neighbors, link) {
@@ -23,15 +34,12 @@ function isNeighborLink(node, link) {
   return link.target.id === node.id || link.source.id === node.id
 }
 
-
 function getNodeColor(node, neighbors) {
   if (Array.isArray(neighbors) && neighbors.indexOf(node.id) > -1) {
     return node.level === 1 ? 'blue' : 'green'
   }
-
   return node.level === 1 ? 'red' : 'gray'
 }
-
 
 function getLinkColor(node, link) {
   return isNeighborLink(node, link) ? 'green' : '#E5E5E5'
@@ -49,29 +57,40 @@ function selectNode(selectedNode) {
   linkElements.attr('stroke', function (link) { return getLinkColor(selectedNode, link) })
 }
 
+/**
+ * ============================================================================
+ * ========================= End Utility Functions ============================
+ * ============================================================================
+ */
 /**============================================================================
- * =============================== Main Function ==============================
+ * ============================= Main Function ================================
  * ============================================================================
  */
 function renderNetworkViz(nodes, links) {
+  // Define width and height for SVG/visualization
   var width = window.innerWidth/1.1
   var height = window.innerHeight/2
   
+  // Select svg from DOM
   var svg = d3.select('svg')
+  
+  // Set width and height of SVG
   svg.attr('width', width).attr('height', height)
   
-  // simulation setup with all forces
+  // Set up link force for simulation
   var linkForce = d3
     .forceLink()
     .id(function (link) { return link.id })
     .strength(function (link) { return link.strength })
   
+  // Set up simulation
   var simulation = d3
     .forceSimulation()
     .force('link', linkForce)
     .force('charge', d3.forceManyBody().strength(-120))
     .force('center', d3.forceCenter(width / 2, height / 2))
   
+  // Determine what happens when dragging and dropping nodes
   var dragDrop = d3.drag().on('start', function (node) {
     node.fx = node.x
     node.fy = node.y
@@ -87,14 +106,17 @@ function renderNetworkViz(nodes, links) {
     node.fy = null
   })
   
+  // Create Link Elements
   var linkElements = svg.append("g")
     .attr("class", "links")
     .selectAll("line")
     .data(links)
     .enter().append("line")
+      .attr("pathLength", "10")
       .attr("stroke-width", 1)
       .attr("stroke", "rgba(50, 50, 50, 0.2)")
   
+  // Create Node Elements
   var nodeElements = svg.append("g")
     .attr("class", "nodes")
     .selectAll("circle")
@@ -104,7 +126,8 @@ function renderNetworkViz(nodes, links) {
       .attr("fill", getNodeColor)
       .call(dragDrop)
       .on('click', selectNode)
-      
+  
+  // Create Node Labels
   var nodeText = svg.append("g")
     .attr("class", "texts")
     .selectAll("text")
@@ -115,6 +138,7 @@ function renderNetworkViz(nodes, links) {
       .attr("dx", 15)
       .attr("dy", 4)
   
+  // Create Link Labels
   var linkText = svg.append("g")
     .attr("class", "texts")
     .selectAll("text")
@@ -125,6 +149,7 @@ function renderNetworkViz(nodes, links) {
       .attr("dx", 0)
       .attr("dy", 0)    
   
+  // Start simulation of network of nodes
   simulation.nodes(nodes).on('tick', () => {
     nodeElements
       .attr('cx', function (node) { return node.x })
@@ -132,15 +157,16 @@ function renderNetworkViz(nodes, links) {
     nodeText
       .attr('x', function (node) { return node.x })
       .attr('y', function (node) { return node.y })
+    linkElements
+    .attr('x1', function (link) { return link.source.x })
+    .attr('y1', function (link) { return link.source.y })
+    .attr('x2', function (link) { return link.target.x })
+    .attr('y2', function (link) { return link.target.y })
     linkText
       .attr('x', function (link) { return (link.source.x+link.target.x)/2 })
       .attr('y', function (link) { return (link.source.y+link.target.y)/2 })
-    linkElements
-      .attr('x1', function (link) { return link.source.x })
-      .attr('y1', function (link) { return link.source.y })
-      .attr('x2', function (link) { return link.target.x })
-      .attr('y2', function (link) { return link.target.y })
   })
   
+  // Start simulation of links connectin the nodes 
   simulation.force("link").links(links)
 }
